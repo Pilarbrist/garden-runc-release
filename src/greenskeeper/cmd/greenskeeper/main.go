8952535ff03777c5e15aaa5c391db1e70cef6840
Danail Branekov <danailster@gmail.com>
2018-10-26 17:39:27 +0300

    WIP: Run Garden as minimus
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

--- src/greenskeeper/cmd/greenskeeper/main.go
+++ src/greenskeeper/cmd/greenskeeper/main.go
@@ -15,9 +15,11 @@ func main() {
 
 	flag.Parse()
 
-	owner := 0
+	ownerUid := 0
+	ownerGid := 0
 	if rootlessMode {
-		owner = mustGetMaximus()
+		ownerUid = mustGetMinimusUid()
+		ownerGid = mustGetMinimusGid()
 	}
 
 	pidFilePath := os.Getenv("PIDFILE")
@@ -28,17 +30,17 @@ func main() {
 
 	directories := []greenskeeper.Directory{
 		greenskeeper.NewDirectoryBuilder(mustGetenv("RUN_DIR")).Mode(0770).Build(),
-		greenskeeper.NewDirectoryBuilder(mustGetenv("GARDEN_DATA_DIR")).Mode(0770).UID(mustResolveUID("vcap")).GID(mustGetMaximus()).Build(),
-		greenskeeper.NewDirectoryBuilder(mustGetenv("LOG_DIR")).Mode(0770).UID(owner).GID(owner).Build(),
-		greenskeeper.NewDirectoryBuilder(mustGetenv("TMPDIR")).Mode(0755).UID(owner).GID(owner).Build(),
-		greenskeeper.NewDirectoryBuilder(mustGetenv("DEPOT_PATH")).Mode(0755).UID(owner).GID(owner).Build(),
-		greenskeeper.NewDirectoryBuilder(mustGetenv("RUNTIME_BIN_DIR")).Mode(0750).GID(mustGetMaximus()).Build(),
+		greenskeeper.NewDirectoryBuilder(mustGetenv("GARDEN_DATA_DIR")).Mode(0770).UID(mustResolveUID("vcap")).GID(mustGetMinimusGid()).Build(),
+		greenskeeper.NewDirectoryBuilder(mustGetenv("LOG_DIR")).Mode(0770).UID(ownerUid).GID(ownerGid).Build(),
+		greenskeeper.NewDirectoryBuilder(mustGetenv("TMPDIR")).Mode(0755).UID(ownerUid).GID(ownerGid).Build(),
+		greenskeeper.NewDirectoryBuilder(mustGetenv("DEPOT_PATH")).Mode(0755).UID(ownerUid).GID(ownerGid).Build(),
+		greenskeeper.NewDirectoryBuilder(mustGetenv("RUNTIME_BIN_DIR")).Mode(0750).GID(mustGetMinimusGid()).Build(),
 		greenskeeper.NewDirectoryBuilder(mustGetenv("GRAPH_PATH")).Mode(0700).UID(mustGetMaximus()).GID(mustGetMaximus()).Build(),
 	}
 
 	if rootlessMode {
-		directories = append(directories, greenskeeper.NewDirectoryBuilder(mustGetenv("XDG_RUNTIME_DIR")).Mode(0700).UID(owner).GID(owner).Build())
-		directories = append(directories, greenskeeper.NewDirectoryBuilder(mustGetenv("GARDEN_ROOTLESS_CONFIG_DIR")).Mode(0700).UID(owner).GID(owner).Build())
+		directories = append(directories, greenskeeper.NewDirectoryBuilder(mustGetenv("XDG_RUNTIME_DIR")).Mode(0700).UID(ownerUid).GID(ownerGid).Build())
+		directories = append(directories, greenskeeper.NewDirectoryBuilder(mustGetenv("GARDEN_ROOTLESS_CONFIG_DIR")).Mode(0700).UID(ownerUid).GID(ownerGid).Build())
 	}
 
 	if err := greenskeeper.CreateDirectories(directories...); err != nil {
@@ -67,6 +69,30 @@ func mustGetMaximus() int {
 	}
 
 	return maximusID
+}
+
+func mustGetMinimusUid() int {
+	minimus := mustGetenv("MINIMUS_UID")
+
+	minimusID, err := strconv.Atoi(minimus)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error converting '%s' string to int", minimus)
+		os.Exit(1)
+	}
+
+	return minimusID
+}
+
+func mustGetMinimusGid() int {
+	minimus := mustGetenv("MINIMUS_GID")
+
+	minimusGID, err := strconv.Atoi(minimus)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error converting '%s' string to int", minimus)
+		os.Exit(1)
+	}
+
+	return minimusGID
 }
 
 func mustResolveUID(username string) int {
